5481ea65ecc7dc3a8e58d5c6968993ebc9fba672
atlas4343 <i.tatsuaki43@gmail.com>
2023-08-01 10:37:45 +0900

    disscytale program added

--- python/scytale.py
+++ python/scytale.py
@@ -15,13 +15,31 @@ def scytale(str1, key):
         new_str += strs
     return new_str
 
-#programming now...
-#def disscytale(str1, key):
-#    return new_str
+#str1 release from scyatle cipher with key
+def disscytale(str1, key):
+    new_str = ""
+    strlist = []
+    key_count = 0
+    #strlist created
+    l_key = len(str1)
+    if l_key % 3 == 0:
+        new_key = l_key // key
+    else:
+        new_key = l_key // key + 1
+    for k in range(new_key):
+        strlist.append("")
+    #distribute str to strlist implement
+    for s in str1:
+        strlist[key_count] += s
+        key_count = (key_count + 1) % new_key
+    # united string of strlist
+    for strs in strlist:
+        new_str += strs
+    return new_str
 
-str1 = "ItsHotToday"
+str1 = "It's hot today."
 key = 3
 str1_scytale = scytale(str1,key)
 print(str1_scytale)
-#str1_disscytale = disceaser(str1_scytale,key)
-#print(str1_disscyatle)
+str1_disscytale = disscytale(str1_scytale,key)
+print(str1_disscytale)
